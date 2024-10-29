9358083e8a3bc7c4ac004e8ba0f1310c8fa46404
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-10-29 15:19:08 +0200

    reference main project with LoggerService

--- EmployeesCompany/obj/Debug/net6.0/EmployeesCompany.AssemblyInfo.cs
+++ EmployeesCompany/obj/Debug/net6.0/EmployeesCompany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeesCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b410200bbc294822e2c6a300adee70a7b616a8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeesCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeesCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
