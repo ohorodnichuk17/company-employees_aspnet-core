0975870e15e38775cf790fb7fc088ac544e2263a
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-11-01 15:59:39 +0200

    add custom csv formatter

--- CompanyEmployees.Presentation/obj/Debug/net6.0/CompanyEmployees.Presentation.AssemblyInfo.cs
+++ CompanyEmployees.Presentation/obj/Debug/net6.0/CompanyEmployees.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployees.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+107158167c8e1c3c6381dacf0decbbb414735805")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef8e45efdc8442dde1879d9ebd1a5bd73537c72")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployees.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployees.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
