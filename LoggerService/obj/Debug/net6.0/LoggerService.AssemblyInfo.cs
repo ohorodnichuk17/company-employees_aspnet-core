ac1f97e4eb908c3278bbd76e75275d5fd1497774
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-11-14 21:12:43 +0200

    implement searching by searchTerm

--- LoggerService/obj/Debug/net6.0/LoggerService.AssemblyInfo.cs
+++ LoggerService/obj/Debug/net6.0/LoggerService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50ed960597abab774e5afb41494b9eab0c4db58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cb287f64cbc7c35757275be84fd7de0065e9160")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 using Contracts;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 using Shared.RequestFeatures;
 
 namespace Repository;
@@ -15,9 +16,9 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
        EmployeeParameters employeeParameters, bool trackChanges)
    {
        var employees = await FindByCondition(
-               e => e.CompanyId.Equals(companyId) && 
-                    (e.Age >= employeeParameters.MinAge && 
-                     e.Age <= employeeParameters.MaxAge), trackChanges)
+               e => e.CompanyId.Equals(companyId), trackChanges)
+           .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+           .Search(employeeParameters.SearchTerm)
            .OrderBy(e => e.Name)
            .ToListAsync();
 
